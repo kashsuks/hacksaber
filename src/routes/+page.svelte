15ce0475a9b9381fd04036d032266271e0e0d8b2
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-08-13 11:34:13 -0600

    feat: basic header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="h-screen font-mono text-white">
+	<img
+		src="lightshow-optimized.gif"
+		class="absolute w-screen object-cover opacity-40 blur-sm"
+		alt="lightshow-bg"
+	/>
+	<div class="absolute left-1/2 mt-32 w-3/4 -translate-x-1/2 transform">
+		<img class="h-auto w-full duration-100 hover:drop-shadow-xl" src="hacksaber.png" alt="logo" />
+	</div>
+
+
+
+</div>
