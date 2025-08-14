4c198caef4f4db13b65252206b47de03c8ae30fd
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-08-14 10:05:22 -0600

    feat: header, instructions, prizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,72 @@
-<script>
-</script>
+<title>Hacksaber!</title>
 
-<div class="h-screen font-mono text-white">
+<div class="relative h-[100vh] overflow-hidden font-mono text-white">
 	<img
 		src="lightshow-optimized.gif"
-		class="absolute w-screen object-cover opacity-40 blur-sm"
+		class="absolute left-0 top-0 h-full w-full object-cover opacity-40 blur-sm"
 		alt="lightshow-bg"
 	/>
-	<div class="absolute left-1/2 mt-32 w-3/4 -translate-x-1/2 transform">
-		<img class="h-auto w-full duration-100 hover:drop-shadow-xl" src="hacksaber.png" alt="logo" />
+	<div class="relative z-10 flex h-full flex-col items-center justify-center px-4 text-center">
+		<img
+			class="h-auto max-w-5xl duration-100 hover:drop-shadow-xl"
+			src="hacksaber.png"
+			alt="logo"
+		/>
+		<h1 class="ml-20 mr-20 mt-8 text-5xl">
+			Ship a Beat Saber modchart, and we'll gift you Beat Saber, BL Premium, or custom merch!
+		</h1>
 	</div>
+</div>
 
+<div class="bg-gradient-to-b from-black to-[#230c46] mb-0 pb-8 pt-8">
+	<h1 class="mt-16 text-center text-8xl text-white">How it works</h1>
 
+	<ol class="ml-100 mr-100 mt-20 list-decimal pb-20 text-center text-3xl">
+		<li class="mb-5">
+			Download <a href="https://cm.topc.at/dl" class="underline">Chromapper</a> and
+			<a
+				href="https://github.com/FallenCharlotte/ChroMapper-PropEdit/releases/tag/v0.12.1"
+				class="underline">PropEdit</a
+			>
+		</li>
+		<li class="mb-5">Pick a song and download it as a .ogg</li>
+		<li class="mb-5">Create a new Chromapper project</li>
+		<li class="mb-5">Make your map's gameplay</li>
+		<li class="mb-5">The fun part — Make a Chroma lightshow!</li>
+		<li class="mb-5">Submit your map in #hacksaber!</li>
+		<li class="">Redeem your prizes!</li>
+	</ol>
+</div>
+<div class="bg-gradient-to-b from-[#230c46] to-black pb-8">
+	<h1 class="text-center text-8xl text-white">Prizes</h1>
+
+	<div class="width-1/2 rounded-sm p-8 text-center text-3xl content-center">
+		<table class="mx-auto">
+			<tbody>
+				<tr class=''>
+					<th class="p-6 outline-white outline">Prize</th>
+					<th class="p-6 outline-white outline">Requirement</th>
+					<th class="p-6 outline-white outline">Details</th>
+				</tr>
+				<tr>
+					<td class="p-8 outline-white outline">★Beat Saber★</td>
+					<td class="p-8 outline-white outline">8 Hours + Chroma/Noodle</td>
+					<td class="p-8 outline-white outline">The game itself! Best if you have a VR but don't own BS</td>
+				</tr>
+				<tr>
+					<td class="p-8 outline-white outline">★BeatLeader Supporter★</td>
+					<td class="p-8 outline-white outline">4 Hours + Chroma</td>
+					<td class="p-8 outline-white outline">BeatLeader premium for if you own Beat Saber and want some extra features around custom maps!</td>
+				</tr>
+				<tr>
+					<td class="p-8 outline-white outline">★Stickers★</td>
+					<td class="p-8 outline-white outline">2 Hours</td>
+					<td class="p-8 outline-white outline">For if you don't have a VR headset at all and don't plan to get one. Custom made stickers!</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<div class="bg-gradient-to-b from-black to-[#230c46] mb-0 pb-8 pt-8">
 
 </div>
