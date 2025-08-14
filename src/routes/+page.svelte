75e665aa7bf600e33713c72a6cdd750ae02bce93
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-08-14 12:30:45 -0600

    feat: faq, styling, cta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,10 +18,10 @@
 	</div>
 </div>
 
-<div class="bg-gradient-to-b from-black to-[#230c46] mb-0 pb-8 pt-8">
-	<h1 class="mt-16 text-center text-8xl text-white">How it works</h1>
+<div class="mb-0 bg-gradient-to-b from-black to-[#230c46] pb-8 pt-8">
+	<h1 class="mt-16 text-center text-8xl text-white">☆How it works☆</h1>
 
-	<ol class="ml-100 mr-100 mt-20 list-decimal pb-20 text-center text-3xl">
+	<ol class="ml-100 mr-100 mt-20 list-decimal pb-10 text-center text-3xl">
 		<li class="mb-5">
 			Download <a href="https://cm.topc.at/dl" class="underline">Chromapper</a> and
 			<a
@@ -37,36 +37,98 @@
 		<li class="">Redeem your prizes!</li>
 	</ol>
 </div>
-<div class="bg-gradient-to-b from-[#230c46] to-black pb-8">
-	<h1 class="text-center text-8xl text-white">Prizes</h1>
 
-	<div class="width-1/2 rounded-sm p-8 text-center text-3xl content-center">
+<div class="bg-gradient-to-b from-[#230c46] to-black pb-8">
+	<hr class="mx-auto w-[80vw] pb-8 pt-4" />
+
+	<h1 class="text-center text-8xl text-white">☆Prizes☆</h1>
+
+	<div class="width-1/2 content-center rounded-sm p-8 text-center text-3xl">
 		<table class="mx-auto">
 			<tbody>
-				<tr class=''>
-					<th class="p-6 outline-white outline">Prize</th>
-					<th class="p-6 outline-white outline">Requirement</th>
-					<th class="p-6 outline-white outline">Details</th>
+				<tr class="">
+					<th class="p-6 outline outline-white">Prize</th>
+					<th class="p-6 outline outline-white">Requirement</th>
+					<th class="p-6 outline outline-white">Details</th>
 				</tr>
 				<tr>
-					<td class="p-8 outline-white outline">★Beat Saber★</td>
-					<td class="p-8 outline-white outline">8 Hours + Chroma/Noodle</td>
-					<td class="p-8 outline-white outline">The game itself! Best if you have a VR but don't own BS</td>
+					<td class="p-8 outline outline-white">★Beat Saber★</td>
+					<td class="p-8 outline outline-white">8 Hours + Chroma/Noodle</td>
+					<td class="p-8 outline outline-white"
+						>The game itself! Best if you have a VR but don't own BS</td
+					>
 				</tr>
 				<tr>
-					<td class="p-8 outline-white outline">★BeatLeader Supporter★</td>
-					<td class="p-8 outline-white outline">4 Hours + Chroma</td>
-					<td class="p-8 outline-white outline">BeatLeader premium for if you own Beat Saber and want some extra features around custom maps!</td>
+					<td class="p-8 outline outline-white">★BeatLeader Supporter★</td>
+					<td class="p-8 outline outline-white">4 Hours + Chroma</td>
+					<td class="p-8 outline outline-white"
+						>BeatLeader premium for if you own Beat Saber and want some extra features around custom
+						maps!</td
+					>
 				</tr>
 				<tr>
-					<td class="p-8 outline-white outline">★Stickers★</td>
-					<td class="p-8 outline-white outline">2 Hours</td>
-					<td class="p-8 outline-white outline">For if you don't have a VR headset at all and don't plan to get one. Custom made stickers!</td>
+					<td class="p-8 outline outline-white">★Stickers★</td>
+					<td class="p-8 outline outline-white">2 Hours</td>
+					<td class="p-8 outline outline-white"
+						>For if you don't have a VR headset at all and don't plan to get one. Custom made
+						stickers!</td
+					>
 				</tr>
 			</tbody>
 		</table>
 	</div>
 </div>
-<div class="bg-gradient-to-b from-black to-[#230c46] mb-0 pb-8 pt-8">
+<div class="mb-0 bg-gradient-to-b from-black to-[#460c3c] pb-8 pt-8">
+	<hr class="mx-auto w-[80vw] pb-8 pt-4" />
+	<h1 class="mb-8 mt-16 text-center text-8xl text-white">☆FAQ☆</h1>
+	<h2 class="mt-6 text-center text-4xl font-bold text-white">What is Beat Saber?</h2>
+	<p class="mb-8 mt-2 text-center text-xl">
+		A VR rhythm game where the player slices cubes to a song!
+	</p>
 
+	<hr class="mx-auto mb-2 w-[20vw] pb-2 pt-4" />
+
+	<h2 class="text-center text-4xl font-bold text-white">Is mapping hard?</h2>
+	<p class="mb-8 mt-2 text-center text-xl">
+		There's only a few basic principles to learn, after that it's smooth sailing!
+	</p>
+
+	<hr class="mx-auto mb-2 w-[20vw] pb-2 pt-4" />
+
+	<h2 class="text-center text-4xl font-bold text-white">How do I sign up?</h2>
+	<p class="mb-8 mt-2 text-center text-xl">Just join #hacksaber and make a map!</p>
+
+	<hr class="mx-auto mb-2 w-[20vw] pb-2 pt-4" />
+
+	<h2 class="text-center text-4xl font-bold text-white">What does BeatLeader Supporter do?</h2>
+	<p class="mb-8 mt-2 text-center text-xl">
+		Gives star ratings for <b>every</b> custom map, and the best custom saber mod available! (reesabers)
+	</p>
+
+	<hr class="mx-auto mb-2 w-[20vw] pb-2 pt-4" />
+
+	<h2 class="text-center text-4xl font-bold text-white">What does BeatLeader Supporter do?</h2>
+	<p class="mb-8 mt-2 text-center text-xl">
+		Gives star ratings for <b>every</b> custom map, and the best custom saber mod available! (reesabers)
+	</p>
+
+	<hr class="mx-auto mb-2 w-[20vw] pb-2 pt-4" />
+
+	<h2 class="text-center text-4xl font-bold text-white">When does this end?</h2>
+	<p class="mb-8 mt-2 text-center text-xl">We're still figuring this out</p>
+
+	<hr class="mx-auto mb-2 w-[20vw] pb-2 pt-4" />
+
+	<h2 class="text-center text-4xl font-bold text-white">How do I submit?</h2>
+	<p class="mb-8 mt-2 text-center text-xl">
+		Send your map's .zip or <a href="https://beatsaver.com/">BeatSaver</a> link in #hacksaber!
+	</p>
+
+	<hr class="mx-auto mt-12 w-[80vw] pb-8 pt-4" />
+
+	<h2 class="mt-4 text-center text-4xl font-bold text-white">Join now! #hacksaber</h2>
+	<div class="mt-10 flex justify-center gap-4">
+		<img src="rednote.png" alt="red note" class="w-32" /> <!--XIAO HONG SHU-->
+		<img src="blunote.png" alt="blu note" class="w-32" />
+	</div>
 </div>
